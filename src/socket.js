dbab77e6ec3e2f9a189ae5ae35a51df198ce5893
Viktor Kondolovskyi <viktor@clioonline.dk>
2018-11-22 14:55:29 +0200

    socket class js doc

--- src/socket.js
+++ src/socket.js
@@ -1,17 +1,28 @@
 const host = 'wss://kondolovskiy.herokuapp.com';
 
+/**
+ * Socket wrapper
+ */
 class Socket {
 
 	constructor() {
 		this._connect();
 	}
 
+	/**
+	 * Socket connector. Create socket instance and bind events to it
+	 * @private
+	 */
 	_connect() {
 		this.socket = new WebSocket(host);
 
 		this._bindEvents();
 	}
 
+	/**
+	 * Bind socket events to socket instance
+	 * @private
+	 */
 	_bindEvents() {
 		this.socket.onopen = this._onOpen;
 		this.socket.onclose = this._onClose.bind(this);
@@ -19,10 +30,19 @@ class Socket {
 		this.socket.onerror = this._onError;
 	}
 
+	/**
+	 * Socket open handler
+	 * @private
+	 */
 	_onOpen() {
 		console.log("Connected");
 	}
 
+	/**
+	 * Socket close handler
+	 * @param event
+	 * @private
+	 */
 	_onClose(event) {
 		if (event.wasClean) {
 			console.log('Connection are closed');
@@ -36,14 +56,28 @@ class Socket {
 		}, 1000); // try to reconnect when connection was closed
 	}
 
+	/**
+	 * Socket message receive handler
+	 * @param event
+	 * @private
+	 */
 	_onMessage(event) {
 		console.log("received data " + event.data);
 	}
 
+	/**
+	 * Socket error handler
+	 * @param error
+	 * @private
+	 */
 	_onError(error) {
 		console.log("Error " + error.message);
 	}
 
+	/**
+	 * Send socket message
+	 * @param message
+	 */
 	send(message) {
 		this.socket.send(message);
 	}
