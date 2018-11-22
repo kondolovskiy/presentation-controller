34e991d6786029bdd2adb1d948874d80533a6221
Viktor Kondolovskyi <viktor@clioonline.dk>
2018-11-22 11:20:57 +0200

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,47 @@
+const socket = new WebSocket('ws://kondolovskiy.herokuapp.com');
+
+socket.onopen = function() {
+	console.log("Connected");
+};
+
+socket.onclose = function(event) {
+	if (event.wasClean) {
+		console.log('Connection are closed');
+	} else {
+		console.log('Bad connection'); // например, "убит" процесс сервера
+	}
+	console.log('Code: ' + event.code + ' reason: ' + event.reason);
+};
+
+socket.onmessage = function(event) {
+	console.log("received data " + event.data);
+};
+
+socket.onerror = function(error) {
+	console.log("Ошибка " + error.message);
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+	const next = document.querySelector('.next');
+	const prev = document.querySelector('.prev');
+
+	prev.addEventListener('click', e => {
+		socket.send('prev');
+	});
+
+	next.addEventListener('click', e => {
+		socket.send('next');
+	});
+
+	const noSleepBtn = document.getElementById('noSleep');
+	const noSleep = new NoSleep();
+
+	function enableNoSleep() {
+		noSleep.enable();
+		noSleepBtn.removeEventListener('click', enableNoSleep, false);
+	}
+
+	noSleepBtn.addEventListener('click', enableNoSleep, false);
+
+	noSleep.disable();
+});
